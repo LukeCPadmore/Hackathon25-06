49900dc0c360c3dd93bca91101de01d2751b5643
Xucas999 <60233653+Xucas999@users.noreply.github.com>
2022-06-25 14:29:13 +0100

    Update Scraping.py

--- Scraping.py
+++ Scraping.py
@@ -3,6 +3,15 @@ from bs4 import BeautifulSoup
 
 r = requests.get('https://www.youtube.com/watch?v=dJToSyr4yvM')
 soup = BeautifulSoup(r.content, 'html.parser')
-metaContent = soup.find_all("meta")
-for i in range(0, len(metaContent)):
-    print(metaContent[i])
+
+arrayOfInfo = []
+
+title = soup.find("meta", property = "og:title")["content"]
+arrayOfInfo.append(title)
+
+metaContent = soup.find_all("meta", property = "og:video:tag")
+for i in metaContent:
+    arrayOfInfo.append(i["content"])
+
+for i in range(0,len(arrayOfInfo)):
+    print(arrayOfInfo[i])
