642d0ede26fef016fc24c91687f45efaa0c165ee
Xucas999 <60233653+Xucas999@users.noreply.github.com>
2022-06-25 18:34:28 +0100

    Update Scraping.py

--- Scraping.py
+++ Scraping.py
@@ -4,8 +4,9 @@ import time
 import requests
 from bs4 import BeautifulSoup
 
-for j in range(0,20):
-    url = "https://www.youtube.com/watch?v=Q-aiMVY4FkM"
+url = "https://www.youtube.com/watch?v=Q-aiMVY4FkM"
+
+for j in range(0,5):
     driver = webdriver.Chrome(executable_path=r"C:\Users\lucas\%Work\Programs\chromedriver.exe")
     driver.get(url)
 
@@ -19,20 +20,36 @@ for j in range(0,20):
         )
 
         for i in range(0, len(results)):
-            if(results[i].get_attribute("href") != None):
-                if(len(results[i].get_attribute("href").split("watch")) > 1):
-                    watchLinks.append(results[i].get_attribute("href"))
+            try:
+                if(results[i].get_attribute("href") != None):
+                    if(len(results[i].get_attribute("href").split("watch")) > 1):
+                        watchLinks.append(results[i].get_attribute("href"))
+            except:
+                None
 
-        r = requests.get('https://www.youtube.com/watch?v=dJToSyr4yvM')
+    r = requests.get(url)
 
-        soup = BeautifulSoup(r.content, 'html.parser')
-        arrayOfInfo = []
+    soup = BeautifulSoup(r.content, 'html.parser')
+    arrayOfInfo = []
 
-        metaContent = [soup.find("meta", property="og:title")]
-        metaContent += soup.find_all("meta", property="og:video:tag")
+    metaContent = [soup.find("meta", property="og:title")]
+    metaContent += soup.find_all("meta", property="og:video:tag")
 
-        for i in range(0, len(metaContent)):
-            metaContent[i] = metaContent[i]["content"]
+    for i in range(0, len(metaContent)):
+        metaContent[i] = metaContent[i]["content"]
+
+
+    print(j , ":", metaContent[0])
+
+    f = open("VideoInfo.txt","a")
+    for info in metaContent:
+        f.write(info + "\n")
+    f.write("--\n")
+    f.close()
+
+
+    url = watchLinks[2]
+    driver.close()
 
 
         print(j , ":", metaContent[0])
